ec048597b950f9faf85505205f1fa14bc47b17ba
Ujwal JV <158304947+Ujwaljv@users.noreply.github.com>
2025-02-06 16:19:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,14 @@ import gdown
 import os
 from PIL import Image  # Import PIL for image processing
 
-# Load the trained model once to avoid reloading on every prediction
+file_id = "14xRMUvDJj8IbxR86H9y4OoBCk8bts_dF"
+url = 'https://drive.google.com/file/d/14xRMUvDJj8IbxR86H9y4OoBCk8bts_dF/view?usp=sharing'
 model_path = "trained_plant_disease_model.keras"
+
+
+if not os.path.exists(model_path):
+    st.warning("Downloading model from Google Drive...")
+    gdown.download(url,model_path,quiet=False)
 model = tf.keras.models.load_model(model_path)
 
 # Function for model prediction
@@ -50,11 +56,4 @@ elif app_mode == "DISEASE RECOGNITION":
             class_name = ['Potato__Early_blight', 'Potato_Late_blight', 'Potato__healthy']
             st.success(f"Model is Predicting it's a {class_name[result_index]}")
             
-file_id = "14xRMUvDJj8IbxR86H9y4OoBCk8bts_dF"
-url = 'https://drive.google.com/file/d/14xRMUvDJj8IbxR86H9y4OoBCk8bts_dF/view?usp=sharing'
-model_path = "trained_plant_disease_model.keras"
 
-
-if not os.path.exists(model_path):
-    st.warning("Downloading model from Google Drive...")
-    gdown.download(url,model_path,quiet=False)
